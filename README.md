8ea24215bed98c5d4661926afe202b720ae050ce
Dimitri C <couwenberg@gmail.com>
2018-09-24 13:29:55 +1000

    feat: style update

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 1.clone this repo.????
 2.install:goto terminal and type 'yarn'.
+3.run server: npm run server.
 
 This project was bootstrapped with create-react-app.
--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+export default class Widget extends Component {
 
-export default (props) => {
-  const { title, icon, degrees, wind, location, unitsType, speed } = props;
-  return (
-    <div>
-        <div className="row">
-            <div className="col-lg-12">{title}</div>
-        </div>
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.degrees!== nextProps.degrees || 
+        this.props.wind !== nextProps.wind ||
+        this.props.title !== nextProps.title;
+    }
 
-        <div className="row">
-            <div className="col-lg-6">
-                <img src={'http://openweathermap.org/img/w/' + icon + '.png'}></img>
+    render() {
+        const { title, icon, degrees, wind, location, unitsType, speed } = this.props;
+        return (
+            <div className="widget">
+                <div className="row">
+                    <div className="col-lg-12 title">{title}</div>
+                </div>
+
+                <div className="row widgettop">
+                    <div className="col-lg-6 topicon">
+                        <img src={'http://openweathermap.org/img/w/' + icon + '.png'}></img>
+                    </div>
+                    <div className="col-lg-6 topdegrees">
+                        {location}
+                        <div className='degrees'>{degrees}&deg;</div>
+                        {wind && <div>Wind{' '}<span className='wind'>{speed}</span> {unitsType === 'metric' ? <span>km/h</span> : <span>mph</span>}</div>}
+                    </div>
+                </div>
             </div>
-            <div className="col-lg-6">
-                {location}
-                <div className='degrees'>{degrees}&deg;</div>
-                {wind && <div>Wind{' '}<span className='wind'>{speed}</span> {unitsType === 'metric' ? <span>km/h</span> : <span>mph</span>}</div>}
-            </div>
-        </div>
-    </div>
-)
+        )
+    }
 }
